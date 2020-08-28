c0a90eb02f351bc1d3d4a6153a4a43462ceb1e49
Tanya S <tanlull@gmail.com>
2020-08-28 10:10:33 +0700

    update

--- TanStragram/src/components/GridImages.vue
+++ TanStragram/src/components/GridImages.vue
@@ -1,46 +1,41 @@
 <template>
-  <div>
-    <div class="ui card" v-for="(userName,i) in userNames" :key="i">
-      <div class="content">
-        <div class="right floated meta">14h</div>
-        <img class="ui avatar image" :src="imgSrc" />
-        <!-- {{apis[i].name}} -->
-      </div>
-      <div class="image">
-        <img :src="imgSrc" />
-      </div>
-      <div class="content">
-        <span class="right floated">
-          <i class="heart outline like icon"></i>
-          17 likes
-        </span>
-        <i class="comment icon"></i>
-        3 comments
-      </div>
-      <div class="extra content">
-        <div class="ui large transparent left icon input">
-          <i class="heart outline icon"></i>
-          <input type="text" placeholder="Add Comment..." />
-        </div>
-      </div>
+    <div id="grid-images">
+        <InstraCard :info="info[0]"></InstraCard>
     </div>
-{{apis[0].img}}
-  </div>
+    
 </template>
 
 <script>
 import axios from 'axios';
+import InstraCard from '@/components/InstraCard';
+
 export default {
   name: "GridImages",
-
+ components: {
+        InstraCard
+    },
   data: function() {
     return {
       imgSrc:"https://github.com/tanlull/test_db/raw/master/images/house1.png",
       userNames: ["Tan", "Lull", "Bingo", "Preme"],
       apis: null,
+      dogs:null,
+      info:[
+          {
+            username: 'TanStragram',
+            avatarImg: 'https://photos.google.com/share/AF1QipNhmCnzFItlJusnzmIfKTGdr0qhgs9Kg5bw-jfAvPnGkRqURdT5iGTQ8PZVT28WtA/photo/AF1QipMBi9h2V5G9wSz8x2MHi1d8QmwoEqt22Uw-3L9F?key=RV9QTEVzaGZwblpKQkR4aTlKbXoxQUd5ZEI3T1F3',
+            image: '',
+            numOfLikes: 77,
+            numOfComment: 99
+         }
+      ]
     }
   },
   mounted: function() {
+    axios
+        .get('https://dog.ceo/api/breed/pomeranian/images/random/10')
+        .then(response => this.dogs = response.data.message)
+
     axios
       .get("http://localhost:3000/api/houses")
       .then((response) => (this.apis = response.data));
